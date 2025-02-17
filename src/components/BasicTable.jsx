a423d56fc07862dcbdb7fcf8dc39b92c6df939d8
MrUnAnonymous <saurabhsapkal16@gmail.com>
2025-02-17 15:16:03 +0530

    added style and made minor logic fixes

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -15,27 +15,32 @@ export default function BasicTable({ data, columns }) {
   const table = useReactTable({
     data,
     columns,
+    state: {
+      sorting: sorting,
+      globalFiltering: filtering,
+    },
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
-    state: {
-      sorting: sorting,
-      globalFilter: filtering,
-    },
-    onSortingChange: setSorting,
     getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
     onGlobalFilterChange: setFiltering,
   });
 
   return (
     <div className="flex flex-col justify-center items-center gap-5 p-5">
-      <input
-        type="text"
-        value={filtering}
-        onChange={(e) => setFiltering(e.target.value)}
-        className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-200 rounded-md"
-        placeholder="Search..."
-      />
+      <div>
+        <label htmlFor="input" className="pr-3 text-md">
+          Search
+        </label>
+        <input
+          type="text"
+          value={filtering}
+          onChange={(e) => setFiltering(e.target.value)}
+          className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-200 rounded-md"
+          placeholder="Search..."
+        />
+      </div>
       <table className="min-w-full divide-y divide-gray-200 shadow-sm rounded-lg overflow-hidden">
         <thead className="bg-gray-50">
           {table.getHeaderGroups().map((headerGroup) => (
@@ -47,17 +52,15 @@ export default function BasicTable({ data, columns }) {
                   className="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
                   <div className="flex items-center">
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
                     <span className="ml-2">
                       {
                         {
                           asc: "🔼",
-                          dsc: "🔽",
+                          desc: "🔽",
                         }[header.column.getIsSorted() ?? null]
                       }
                     </span>
@@ -72,7 +75,10 @@ export default function BasicTable({ data, columns }) {
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
+                <td
+                  key={cell.id}
+                  className="px-6 py-4 whitespace-nowrap text-sm text-gray-700"
+                >
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
@@ -82,27 +88,31 @@ export default function BasicTable({ data, columns }) {
       </table>
       <div className="flex items-center gap-2">
         <button
-          className="cursor-pointer px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed"
+          className="px-4 py-2 text-sm font-medium bg-blue-500 text-white rounded-md disabled:bg-gray-300"
+          disabled={table.getState().pagination.pageIndex === 0}
           onClick={() => table.setPageIndex(0)}
         >
           First Page
         </button>
         <button
-          className="cursor-pointer px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed"
+          className="px-4 py-2 text-sm font-medium bg-blue-500 text-white rounded-md disabled:bg-gray-300"
           disabled={!table.getCanPreviousPage()}
           onClick={() => table.previousPage()}
         >
-          Previous Page
+          👈
         </button>
         <button
-          className="cursor-pointer px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed"
+          className="px-4 py-2 text-sm font-medium bg-blue-500 text-white rounded-md disabled:bg-gray-300"
           disabled={!table.getCanNextPage()}
           onClick={() => table.nextPage()}
         >
-          Next Page
+          👉
         </button>
         <button
-          className="cursor-pointer px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed"
+          className="px-4 py-2 text-sm font-medium bg-blue-500 text-white rounded-md disabled:bg-gray-300"
+          disabled={
+            table.getState().pagination.pageIndex === table.getPageCount() - 1
+          }
           onClick={() => table.setPageIndex(table.getPageCount() - 1)}
         >
           Last Page
